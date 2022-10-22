4a140609e26b79da267706482102fd348a30d274
Taomihog <taomihog@gmail.com>
2022-10-22 02:04:46 -0700

    My first Java project and my first to commit using IntelliJ IDEA

--- src/com/example/helloworld/HellowWorld.java
+++ src/com/example/helloworld/HellowWorld.java
@@ -3,7 +3,6 @@ package com.example.helloworld;
 public class HellowWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world! My first Java project.");
-
-
+        System.out.println(123);
     }
 }

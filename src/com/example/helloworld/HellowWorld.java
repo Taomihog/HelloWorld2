4792d8a28e7a56befbdb8c443adf73f1f2d9c220
Taomihog <taomihog@gmail.com>
2022-10-22 02:06:50 -0700

    Use Git desktop to commit

--- src/com/example/helloworld/HellowWorld.java
+++ src/com/example/helloworld/HellowWorld.java
@@ -4,5 +4,6 @@ public class HellowWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world! My first Java project.");
         System.out.println(123);
+        System.out.println('s');
     }
 }
